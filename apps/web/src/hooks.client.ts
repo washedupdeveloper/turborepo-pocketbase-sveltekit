a3d07c598220ad421c42b188e6a6b62eda6e9c37
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-07 02:28:19 +0100

    removed unnecessary user logic

--- apps/web/src/hooks.client.ts
+++
@@ -1,8 +0,0 @@
-import { user } from '$lib/stores';
-import { pb } from '$lib/utils';
-
-pb.authStore.loadFromCookie(document.cookie);
-pb.authStore.onChange(() => {
-  user.set(pb.authStore.model);
-  document.cookie = pb.authStore.exportToCookie({ httpOnly: false, secure: false });
-});
--- apps/web/src/lib/stores.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-import { pb } from './utils';
-
-/**
- * The current user, if one is logged in.
- */
-export const user = writable(pb.authStore.model);
