5b5497621f9348f5c9351ff894d90d98e6c2ac3e
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-10 10:18:07 +0100

    added required & italic error message to input field

--- apps/web/src/lib/ui/InputField.svelte
+++ apps/web/src/lib/ui/InputField.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
-	import type { HTMLInputTypeAttribute } from 'svelte/elements';
-	export let type: HTMLInputTypeAttribute;
-	export let name: string;
-	export let errors: string | string[] | undefined;
+  import type { HTMLInputTypeAttribute } from 'svelte/elements';
+  export let type: HTMLInputTypeAttribute;
+  export let name: string;
+  export let errors: string | string[] | undefined;
 </script>
 
 <div class="form-control w-full max-w-md">
-	<label for={name} class="label font-medium pb-1">
-		<span class="label-text capitalize">{name}</span>
-	</label>
-	<input
-		{type}
-		{name}
-		class="input input-bordered w-full max-w-md mb-2"
-		class:input-error={errors}
-	/>
-	{#if errors}
-		<span class="text-error">{typeof errors === 'string' ? errors : errors[0]}</span>
-	{:else}
-		<span class="text-error invisible opacity-0">hidden</span>
-	{/if}
+  <label for={name} class="label font-medium pb-1">
+    <span class="label-text capitalize">{name}</span>
+  </label>
+  <input {type} {name} required class="input input-bordered w-full max-w-md mb-2" class:input-error={errors} />
+  {#if errors}
+    <span class="text-error italic capitalize-first">{typeof errors === 'string' ? errors : errors[0]}</span>
+  {:else}
+    <span class="text-error invisible">hidden</span>
+  {/if}
 </div>
