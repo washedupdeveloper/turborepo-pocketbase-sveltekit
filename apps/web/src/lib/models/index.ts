7cd9df8fd6f33fd7d8157da919b608b52e20d820
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-10 01:58:37 +0100

    simplified lib structure

---
+++ apps/web/src/lib/models/index.ts
@@ -0,0 +1,35 @@
+import { Lengths } from '$lib/utils';
+import { z } from 'zod';
+
+// TODO implement password complexity - https://github.com/dropbox/zxcvbn
+
+export const loginFormSchema = z
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(Lengths.password.min, `must be at least ${Lengths.password.min} characters`)
+      .max(Lengths.password.max, `must be at most ${Lengths.password.max} characters`)
+  })
+  .required();
+
+export type LoginForm = z.infer<typeof loginFormSchema>;
+
+export const registerFormSchema = loginFormSchema
+  .extend({
+    passwordConfirm: z
+      .string()
+      .min(Lengths.password.min, `must be at least ${Lengths.password.min} characters`)
+      .max(Lengths.password.max, `must be at most ${Lengths.password.max} characters`)
+  })
+  .superRefine((form, ctx) => {
+    if (form.password !== form.passwordConfirm) {
+      ctx.addIssue({
+        path: ['passwordConfirm', 'password'],
+        code: z.ZodIssueCode.custom,
+        message: 'passwords must match'
+      });
+    }
+  });
+
+export type RegisterForm = z.infer<typeof registerFormSchema>;
--- apps/web/src/lib/utils/index.ts
+++ apps/web/src/lib/utils/index.ts
@@ -2,4 +2,8 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import { customAlphabet } from 'nanoid';
 import PocketBase from 'pocketbase';
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
-export const uniqueId = customAlphabet('1234567890abcdef', 10);
+export const Lengths = {
+  password: { min: 6, max: 32 }
+};
+
+export const uniqueId = customAlphabet('1234567890abcdefghijk');
