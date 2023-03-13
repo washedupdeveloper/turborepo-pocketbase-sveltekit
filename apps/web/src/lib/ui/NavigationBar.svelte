60ac714c9330ec20506cde93e0154d9ebf78ca32
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-13 03:09:42 +0100

    fixed up styling in nav bar

--- apps/web/src/lib/ui/NavigationBar.svelte
+++ apps/web/src/lib/ui/NavigationBar.svelte
@@ -1,63 +1,58 @@
 <script lang="ts">
-	import { applyAction, enhance } from '$app/forms';
-	import Icon from './Icon.svelte';
-	import ThemeSwitch from './ThemeSwitch.svelte';
-	export let user: import('pocketbase').default['authStore']['model'];
+  import { applyAction, enhance } from '$app/forms';
+  import Icon from './Icon.svelte';
+  import ThemeSwitch from './ThemeSwitch.svelte';
+  export let user: import('pocketbase').default['authStore']['model'];
+  import { page } from '$app/stores';
+  $: console.log($page.url.pathname);
 </script>
 
 <div class="navbar bg-base-100">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<button class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</button>
-			<ul class="menu menu-compact dropdown-content mt-3 shadow bg-base-100 rounded-box w-52">
-				<li>
-					<span><Icon type="home" size={24} />Home</span>
-				</li>
-				<li><span>Element 2</span></li>
-			</ul>
-		</div>
-		<a href="." class="text-xl">Application Name</a>
-	</div>
-	<div class="navbar-center hidden lg:flex">
-		<ul class="menu menu-horizontal px-1">
-			<li><span><Icon type="home" size={24} />Home</span></li>
-			<li><span>Element 2</span></li>
-		</ul>
-	</div>
-	<div class="navbar-end space-x-4">
-		<ul class="menu menu-horizontal">
-			{#if user}
-				<li>{user.email}</li>
-				<li>
-					<form
-						method="POST"
-						action="/logout"
-						use:enhance={() => {
-							return async ({ result }) => await applyAction(result);
-						}}
-					>
-						<button>Log out</button>
-					</form>
-				</li>
-			{:else}
-				<li><a href="/login">Log in</a></li>
-				<li><a href="/register">Register</a></li>
-			{/if}
-		</ul>
-		<ThemeSwitch />
-	</div>
+  <div class="navbar-start">
+    <div class="dropdown">
+      <button class="btn btn-ghost lg:hidden">
+        <Icon name="hamburger" />
+      </button>
+      <ul class="menu menu-compact dropdown-content mt-2 shadow bg-base-100 rounded-md">
+        <li class:bordered={$page.url.pathname === '/'}>
+          <a href="." class="capitalize"><Icon name="home" />home</a>
+        </li>
+        <li class:bordered={$page.url.pathname === '/dashboard'}>
+          <a href="/dashboard" class="capitalize"><Icon name="view-dashboard-variant-outline" />dashboard</a>
+        </li>
+      </ul>
+    </div>
+    <a href="." class="text-xl capitalize">svecket</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal space-x-2">
+      <li class:active={$page.url.pathname === '/'}><a href="." class="capitalize"><Icon name="home" />home</a></li>
+      <li class:active={$page.url.pathname === '/dashboard'}>
+        <a href="/dashboard" class="capitalize"><Icon name="view-dashboard-variant-outline" />dashboard</a>
+      </li>
+    </ul>
+  </div>
+  <div class="navbar-end space-x-2">
+    <ul class="menu menu-horizontal space-x-2">
+      {#if user}
+        <li class="content-center">{user.email}</li>
+        <li>
+          <form
+            class="p-0"
+            method="POST"
+            action="/logout"
+            use:enhance={() => {
+              return async ({ result }) => await applyAction(result);
+            }}
+          >
+            <button class="btn btn-outline btn-error capitalize"><Icon name="logout" />log out</button>
+          </form>
+        </li>
+      {:else}
+        <li><a href="/login" class="capitalize"><Icon name="login" />log in</a></li>
+        <li><a href="/register" class="capitalize"><Icon name="account-plus-outline" />register</a></li>
+      {/if}
+    </ul>
+    <ThemeSwitch />
+  </div>
 </div>
