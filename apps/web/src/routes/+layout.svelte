b6c5b846989a2aa2df573bf95f4a5a3337fcf824
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-13 03:10:56 +0100

    added toast library

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import NavigationBar from '$lib/ui/NavigationBar.svelte';
-	import '../app.css';
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
-	import type { PageData } from './$types';
-	export let data: PageData;
+  import NavigationBar from '$lib/ui/NavigationBar.svelte';
+  import '../app.css';
+  import { onMount } from 'svelte';
+  import { themeChange } from 'theme-change';
+  import type { PageData } from './$types';
+  export let data: PageData;
+  import { Toaster } from 'svelte-french-toast';
 
-	onMount(() => {
-		themeChange(false);
-	});
+  onMount(() => {
+    themeChange(false);
+  });
 </script>
 
 <div>
-	<NavigationBar user={data.user} />
-	<main class="mx-4">
-		<slot />
-	</main>
+  <Toaster />
+  <NavigationBar user={data.user} />
+  <main class="mx-4">
+    <slot />
+  </main>
 </div>
