18513c6af2b42d983d9a0a346be74e6050a76535
Asger Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-03-13 03:10:35 +0100

    prevented users not logged in from accessing /logout

--- apps/web/src/routes/logout/+page.server.ts
+++ apps/web/src/routes/logout/+page.server.ts
@@ -1,6 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 import http from 'http-status';
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+
+// If there is not a current auth session, redirect away from logout page
+export const load: PageServerLoad = ({ locals }) => {
+  if (!locals.pb.authStore.isValid) {
+    throw redirect(http.SEE_OTHER, '/');
+  }
+};
 
 export const actions: Actions = {
   default: async ({ locals }) => {
